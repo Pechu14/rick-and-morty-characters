b4f6492ab7f46470e9f32687e57439b9ceadf6e8
Pechu14 <jesus.gil.santos1992@gmail.com>
2024-06-20 14:08:32 +0200

    rym terminado

---
+++ js/script.js
@@ -0,0 +1,50 @@
+
+let paginaActual = 1
+
+const nextPage = document.getElementById('next-page')
+nextPage.addEventListener('click', () => { 
+    paginaActual = paginaActual + 1;
+    fetchPersonajes(paginaActual)
+})
+
+const prevPage = document.getElementById('prev-page')
+prevPage.addEventListener('click', () => { 
+    if (paginaActual > 1 ) {
+        paginaActual = paginaActual - 1;
+        fetchPersonajes(paginaActual)
+    }
+})
+
+const mostartEnPantalla = (personajes) => {
+    const divPersonajes = document.getElementById('divPersonajes');
+
+    let totalDivs =''
+    for (let personaje of personajes) {
+        let  divPersonaje = '<div> <img width=200px src=' + personaje.image + '>  </img> <h3>' + personaje.name + '</h3><h3>' + personaje.species + '</h3></div>'; 
+        totalDivs = totalDivs + divPersonaje;
+    }
+    divPersonajes.innerHTML = totalDivs;
+}
+
+function fetchPersonajes(pagina) {
+    fetch('https://rickandmortyapi.com/api/character/?page=' + pagina)
+        .then((response) => {
+            if (!response.ok) {
+            throw new Error('solicitud erronea');
+            }
+            return response.json();
+        })
+        .then((response) => {
+            console.log("🚀 ~ .then ~ response:", response)
+            let personajes = response.results;
+            mostartEnPantalla(personajes)
+        })
+        .catch((error) => {
+            console.error(error)
+            divPersonajes.innerText = 'ha habido un error con la Api: ' + error;
+        });
+}
+
+fetchPersonajes(paginaActual)
+
+
